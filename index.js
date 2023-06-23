3ae80756c9d4b23279b2c92738fbe9e9ac367750
Mohammad Bozorgi <mbozorgi604@gmail.com>
2023-06-23 18:24:55 +0330

    remove spacong

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let counter = 0
 
 window['handleIncrease'] = handleIncrease
-
 window['handleDecrease'] = handleDecrease
 
 function handleIncrease() {
     counter += 1
     document.getElementById('counterDiv').innerHTML = `counter: ${counter}`
 }
-
 function handleDecrease() {
     counter -= 1
     document.getElementById('counterDiv').innerHTML = `counter: ${counter}`
